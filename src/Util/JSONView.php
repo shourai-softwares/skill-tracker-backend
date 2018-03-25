8919560467ffb8e3f254d5c544f40b1b05919f3d
Duilio Felix <duilio@lipperhub.com>
2018-03-25 15:26:30 -0300

    Adding env to allowed origin

--- src/Util/JSONView.php
+++ src/Util/JSONView.php
@@ -28,7 +28,7 @@ class JSONView
     private function createView($json, $status)
     {
         return View::create($json, $status)
-            ->setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->setHeader('Access-Control-Allow-Origin', getenv('ALLOWED_ORIGIN'))
             ->setHeader('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
