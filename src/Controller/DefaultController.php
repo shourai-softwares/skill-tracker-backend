e6903494238fb718e76f9d88b857366366db8d2e
Duilio Felix <duilio@lipperhub.com>
2018-03-25 12:37:28 -0300

    Switching index to REST bundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,14 +1,13 @@
 <?php
 namespace App\Controller;
 
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends FOSRestController
 {
     /**
-     * @Route("/")
+     * @Rest\Get("/")
      */
     public function indexAction() {
         return $this->view('ola', 200);
